60000869ce00f95beb8684c173b04a86fe85336d
root <root@david.tango.com>
2021-07-27 00:14:35 -0500

    Paneles

---
+++ src/app/controls/panel/panel.component.ts
@@ -0,0 +1,25 @@
+import { Component, OnInit,Input } from '@angular/core';
+
+const PaddingConfig = {
+  narrow: '15px',
+  medium: '25px',
+  wide: '35px'
+}
+
+@Component({
+  selector: 'app-panel',
+  templateUrl: './panel.component.html',
+  styleUrls: ['./panel.component.scss']
+})
+export class PanelComponent implements OnInit {
+  @Input() padding : keyof typeof PaddingConfig = 'medium';
+
+  getPadding() {
+    return PaddingConfig[this.padding]; 
+  }
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
